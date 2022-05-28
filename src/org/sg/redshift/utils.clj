ca0cdb3a9cb5291cbf7a57758cfcab583a6aeacf
Benj <Benjamin.Schwerdtner@gmail.com>
2022-05-28 12:12:26 +0200

    Add param util

--- src/org/sg/redshift/utils.clj
+++ src/org/sg/redshift/utils.clj
@@ -1,1 +1,16 @@
 (ns org.sg.redshift.utils)
+
+(defn
+  parameters
+  [parameters-map]
+  (into
+   []
+   (map
+    (fn [[k v]] {:name k :value (str v)}))
+   parameters-map))
+
+(comment
+  (assert
+   (=
+    [{:name "limit" :value (str 9)}]
+    (parameters {"limit" "9"}))))
